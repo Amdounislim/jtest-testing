12d7c94e6cc09b77e3d8a17e3310321d36051206
Amdounislim <amdouni.slim@gmail.com>
2021-08-29 18:37:13 +0100

    counter

---
+++ src/problem-solving/04_async-counter.js
@@ -0,0 +1,30 @@
+/**
+ * Export a function named 'count'.
+ * It should call the given callback every 10th of a second with a increment from given start to end integer
+ * Return a function to prematurely cancel the counting process.
+ *
+ * @param {number} start - Given to callback first
+ * @param {number} end - Stop when count reached this value
+ * @param {function(number): void} callback - Called after each 100ms with an increment
+ * @returns {function(): void} - Cancel countdown function
+ */
+
+
+ const _cancelIntervall = (intervall) => clearInterval(intervall);
+
+ export const count = (start, end, callback) => {
+     let counter = start;
+     let intervall = null;
+ 
+     intervall = setInterval(() => {
+         if (counter === end) {
+             _cancelIntervall(intervall);
+         }
+         callback(counter);
+         counter++;
+     }, 100);
+ 
+     return ()=>_cancelIntervall(intervall);
+ };
+ 
+ 
